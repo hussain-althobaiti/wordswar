bf14624b6b1914352c96cf01409b7ae1dc7cbe69
hussain-althobaiti <1@hussain-althobaiti.sa>
2019-11-18 17:13:07 +0300

    v 1.1

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 var wordlist =[ "go","once","said","where","all","blue","family","she","hold","right","a","three","you","animal","call","brown","father","read","hurt","room"];
 var data="";
 var score =0;
+var highScore;
 var trys=3;
 var word;
 $(document).ready(function(){
@@ -16,9 +17,10 @@ $(document).ready(function(){
         word=wordlist[Random];
         $('#words').html("<h1>"+wordlist[Random]+"</h1>");
         data = $("#mainInputBox").val();
-
     };
+
     nextword();
+    
     $("#mainInputBox").keypress(function(e){
         var keycode = (e.keyCode ? e.keyCode : e.which);
         if (keycode == '13') {
@@ -30,15 +32,26 @@ $(document).ready(function(){
                 $('#score').html("<h2>score : "+score+"</h2>");
                 nextword();
                 $("#mainInputBox").val("");
+                $('#mainInputBox').css("color","#00FF41");
+
             }else{
                 trys -=1;
+                checktrys(trys);
                 $('#trys').html("<h2>trys : "+trys+"</h2>");
+                $('#mainInputBox').css("color","red");
             };
         }
     });
 
-    if(trys==0){
-        
+    function checktrys(tryCount){
+        if(tryCount === 0){
+            alert("Game Over"+" your score is "+score+" the high Score is "+highScore);
+            score=0;
+            trys=3;
+            $('#score').html("<h2>score : "+score+"</h2>");
+            location.reload(true);
+            //setInterval() Method
+        }
     }
 
 
