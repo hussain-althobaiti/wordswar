aba92673f923ff67fd307fece1432e2454edc15f
hussain-althobaiti <1@hussain-althobaiti.sa>
2019-12-10 18:16:53 +0300

    V 4.1

--- js/GameOver.js
+++ js/GameOver.js
@@ -1,11 +1,9 @@
 
-
-
 function GameOver(){
    var HighScore = localStorage.getItem("HighScore");
    var lastScore= localStorage.getItem("lastScore");
-    $('#score').html('<h1>your score is '+lastScore+'</h1>');
-    $('#HighScore').html('<h1>your Highest Score is '+HighScore+'</h1>');
+    $('#score').html('<h1>Your score is '+lastScore+'</h1>');
+    $('#HighScore').html('<h1>Your Highest Score is '+HighScore+'</h1>');
 
 }
 
